84500a3444a9809d543b38d539c005023eeb9976
Ben Bradford <ben.bradford@king.com>
2018-06-18 15:23:12 +0100

    adding dice images

--- src/DieView.tsx
+++ src/DieView.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
 import Die from './Die'
+import die1 from './die1.png';
+import die2 from './die2.png';
+import die3 from './die3.png';
+import die4 from './die4.png';
+import die5 from './die5.png';
+import die6 from './die6.png';
 
 class DieView extends React.Component<any, any>{
 
@@ -14,9 +20,24 @@ class DieView extends React.Component<any, any>{
     }
   
     public render() {
+        
         return (
-            <code>Die {this.num} = {this.die.value()} </code>
+            <p><code>Die {this.num} </code>
+            <img src={this.image()} className="Dice-Img"/>
+            </p>
         );
+    }
+
+    private image() : any {
+        let dImage : any = die1;
+        switch (this.die.value()) {
+            case 2: dImage = die2; break;
+            case 3: dImage = die3; break;
+            case 4: dImage = die4; break;
+            case 5: dImage = die5; break;
+            case 6: dImage = die6; break;
+        }
+        return dImage;
     }
 }
 
